45f0ccf104805384023abdf7ca2ac71af897cdce
Bruno Sosa <sosabruno3384@gmail.com>
2023-08-04 23:12:30 -0300

    Primer Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html, js}"],
+  theme: {
+    extend: {},
+    colors: {
+      green: "#8FA206",
+      blue: "#61AEC9",
+      red: "#CC2D4A",
+      while: "#FFFFFF",
+      black: "#000000",
+    }
+  },
+  plugins: [],
+}
+
